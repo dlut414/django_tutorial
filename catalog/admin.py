528e13ecc663bfffce458917d579dfbf82e49a8d
dlut414 <hfy8373@gmail.com>
2017-10-18 19:35:14 +0900

    part3

---
+++ catalog/admin.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.contrib import admin
+
+# Register your models here.
+from .models import Author, Genre, Book, BookInstance, Language
+
+admin.site.register(Book);
+admin.site.register(Author);
+admin.site.register(Genre);
+admin.site.register(BookInstance);
+admin.site.register(Language);
