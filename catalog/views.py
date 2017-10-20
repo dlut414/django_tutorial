3bba1dae1ef63ed98721105a2a14414246ad62ea
dlut414 <hfy8373@gmail.com>
2017-10-21 02:28:51 +0900

    part 7 sessions

--- catalog/views.py
+++ catalog/views.py
@@ -19,6 +19,10 @@ def index(request):
     num_genres = Genre.objects.count();
     num_book_test = Book.objects.filter(title__contains='test').count();
 
+    # Number of visits to this view, as counted in the session variable.
+    num_visits = request.session.get('num_visits', 0);
+    request.session['num_visits'] = num_visits + 1;
+
     CONTEXT = {
         'num_books':num_books,
         'num_instances':num_instances,
@@ -26,6 +30,7 @@ def index(request):
         'num_authors':num_authors,
         'num_genres':num_genres,
         'num_book_test':num_book_test,
+        'num_visits':num_visits,
     };
     # Render the HTML template index.html with the data in the context variable
     return render(
