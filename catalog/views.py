e9232d13685322c349bc7a9df062f9ee80e2657d
dlut414 <hfy8373@gmail.com>
2017-10-23 18:32:31 +0900

    finished part9

--- catalog/views.py
+++ catalog/views.py
@@ -6,6 +6,14 @@ from .models import Book, Author, BookInstance, Genre, Language
 from django.views import generic
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.contrib.auth.mixins import PermissionRequiredMixin
+from django.contrib.auth.decorators import permission_required
+
+from django.shortcuts import get_object_or_404
+from django.http import HttpResponseRedirect
+from django.core.urlresolvers import reverse
+import datetime
+
+from .forms import RenewBookForm
 
 # Create your views here.
 def index(request):
@@ -92,3 +100,64 @@ class LoanedBooksByLabrarianListView(PermissionRequiredMixin, generic.ListView):
     def get_queryset(self):
         return BookInstance.objects.filter(status__exact='o').order_by('due_back');
 
+@permission_required('catalog.can_mark_returned')
+def renew_book_librarian(request, pk):
+    book_inst=get_object_or_404(BookInstance, pk = pk)
+
+    # If this is a POST request then process the Form data
+    if request.method == 'POST':
+
+        # Create a form instance and populate it with data from the request (binding):
+        form = RenewBookForm(request.POST)
+
+        # Check if the form is valid:
+        if form.is_valid():
+            # process the data in form.cleaned_data as required (here we just write it to the model due_back field)
+            book_inst.due_back = form.cleaned_data['renewal_date']
+            book_inst.save()
+
+            # redirect to a new URL:
+            return HttpResponseRedirect(reverse('all-loaned') )
+
+    # If this is a GET (or any other method) create the default form.
+    else:
+        proposed_renewal_date = datetime.date.today() + datetime.timedelta(weeks=3)
+        form = RenewBookForm(initial={'renewal_date': proposed_renewal_date,})
+
+    return render(request, 'catalog/book_renew_librarian.html', {'form': form, 'bookinst':book_inst})
+
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.urls import reverse_lazy
+from .models import Author
+from .models import Book
+
+class AuthorCreate(PermissionRequiredMixin, CreateView):
+    model = Author
+    permission_required = 'catalog.can_mark_returned';
+    fields = '__all__';
+    initial={'date_of_death':'12/10/2016',};
+
+class AuthorUpdate(PermissionRequiredMixin, UpdateView):
+    model = Author;
+    permission_required = 'catalog.can_mark_returned';
+    fields = ['first_name','last_name','date_of_birth','date_of_death'];
+
+class AuthorDelete(PermissionRequiredMixin, DeleteView):
+    model = Author;
+    permission_required = 'catalog.can_mark_returned';
+    success_url = reverse_lazy('authors');
+
+class BookCreate(PermissionRequiredMixin, CreateView):
+    model = Book
+    permission_required = 'catalog.can_mark_returned';
+    fields = '__all__';
+
+class BookUpdate(PermissionRequiredMixin, UpdateView):
+    model = Book;
+    permission_required = 'catalog.can_mark_returned';
+    fields = '__all__';
+
+class BookDelete(PermissionRequiredMixin, DeleteView):
+    model = Book;
+    permission_required = 'catalog.can_mark_returned';
+    success_url = reverse_lazy('books');
